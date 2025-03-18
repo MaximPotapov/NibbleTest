afd8b168690e073a0659656b01f97b11908a2e2f
mpotapov <mpotapovbusiness@gmail.com>
2025-03-18 17:13:36 +0200

    Initial Commit

---
+++ NibbleTestApp/NibbleTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NibbleTestAppApp.swift
+//  NibbleTestApp
+//
+//  Created by Maxim Potapov on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NibbleTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
